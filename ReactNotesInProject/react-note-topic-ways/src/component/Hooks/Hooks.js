3f2f1a6bb8622e1a234cac2394b32bfce7752634
Ranjit Gajanan Kale <119128221+Ranjitk0001@users.noreply.github.com>
2024-12-31 22:15:22 +0530

    React :- Hooks (UseState using string)

--- ReactNotesInProject/react-note-topic-ways/src/component/Hooks/Hooks.js
+++ ReactNotesInProject/react-note-topic-ways/src/component/Hooks/Hooks.js
@@ -10,16 +10,28 @@ const [count,setCount]=useState(10)
 
 const countUp=()=>{
   setCount(count+1);
+
 }
 
 const countDown=()=>{
   setCount(count-1);
+}
+// Name
+const [name,setName]=useState("Hello from React js");
+const showName=()=>{
+  setName("js");
 }
   return (
     <>
       <button onClick={countUp}>Counter Up</button>
       <h1>{count}</h1>
       <button onClick={countDown}>Counter Down</button>
+
+
+
+      <h1>Name</h1>
+      <h1>{name}</h1>
+      <button onClick={showName}>React</button>
     </>
   )
 }
