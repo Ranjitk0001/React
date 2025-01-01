1aedccea53473d1ffbcd42f8f2bfb9dc39c4407f
Ranjit Gajanan Kale <119128221+Ranjitk0001@users.noreply.github.com>
2025-01-01 23:00:14 +0530

    after merge update

--- ReactNotesInProject/react-note-topic-ways/src/component/Home.js
+++ ReactNotesInProject/react-note-topic-ways/src/component/Home.js
@@ -1,12 +1,5 @@
 import React from 'react'
-<<<<<<< HEAD
 
-const Home = () => {
-  return (
-    <div>
-      Home
-    </div>
-=======
 import RouteNavbar from './Routing/RouteNavbar'
 
 const Home = () => {
@@ -15,7 +8,7 @@ const Home = () => {
     <RouteNavbar/>
     <h1>Home page</h1>
     </>
->>>>>>> demo
+
   )
 }
 
