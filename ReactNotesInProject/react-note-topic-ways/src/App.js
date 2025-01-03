e0d23f3757c8243e47eed631e51e169e078fe8b9
Ranjit Gajanan Kale <119128221+Ranjitk0001@users.noreply.github.com>
2025-01-03 19:17:52 +0530

    UseContext

--- ReactNotesInProject/react-note-topic-ways/src/App.js
+++ ReactNotesInProject/react-note-topic-ways/src/App.js
@@ -15,31 +15,46 @@ import One from "./component/Hooks/useContext/One";
 // import Api from "./component/Api/Api";
 // import Api2 from "./component/Api/Api2";
 
+import { createContext } from "react";
+
+
+const firstName = createContext();
+const lastName = createContext();
 function App() {
   return (
     <>
+      <firstName.Provider value={"Ranjit"}>
+        <lastName.Provider value={"Kale"}>
+
+          <Router>
+            <Routes>
+              <Route path="/hooks" element={<Parent />} />
+              {/* <Route path="/hooks" element={<Hooks />} /> */}
+              <Route path="/" element={<Home />} />
+              <Route path="/*" element={<PageNoteFound />} />
+              <Route path="/props" element={<Variables />} />
+              <Route path="/about" element={<About />}>
+                <Route path="/about/contact-Us" element={<ContactUs />} />
+              </Route>
+              <Route path="/api" element={<Api4 />} />
+              <Route path="/movies" element={<MoviesApi />} />
+              <Route path="/PropDrilling" element={<One />} />
+            </Routes>
+          </Router>
+
+
+        </lastName.Provider>
+      </firstName.Provider>
+
       {/* <h1>React Notes</h1> */}
 
       {/* <h1>React Notes</h1>
   <Hooks></Hooks> */}
 
-      <Router>
-        <Routes>
-          <Route path="/hooks" element={<Parent/>} />
-          {/* <Route path="/hooks" element={<Hooks />} /> */}
-          <Route path="/" element={<Home />} />
-          <Route path="/*" element={<PageNoteFound />} />
-          <Route path="/props" element={<Variables />} />
-          <Route path="/about" element={<About />}>
-            <Route path="/about/contact-Us" element={<ContactUs />} />
-          </Route>
-          <Route path="/api" element={<Api4 />} />
-          <Route path="/movies" element={<MoviesApi />} />
-          <Route path="/PropDrilling" element={<One />} />
-        </Routes>
-      </Router>
+
     </>
   );
 }
 
 export default App;
+export{firstName,lastName};
--- ReactNotesInProject/react-note-topic-ways/src/component/Hooks/useContext/One.js
+++ ReactNotesInProject/react-note-topic-ways/src/component/Hooks/useContext/One.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import Two from './Two'
 import RouteNavbar from '../../Routing/RouteNavbar'
-
+import { useContext } from 'react'
+import { firstName,lastName } from '../../../App'
 const One = () => {
+
+    const fname=useContext(firstName);
+    const lname=useContext(lastName);
+
     const name="Ranjit"
   return (
     <>
     <RouteNavbar/>
-         <h1>This is Third Page</h1> 
-
+         <h1>This is One Page</h1> 
+         <h2>First name is <b>{fname} </b>and the Last name is <b>{lname} </b></h2>
          <hr/>
          <Two name1={name}/>
+
     </>
   )
 }
