7a5ca7993408a5c112724e7e3b8e0c156e093a96
Ranjit Gajanan Kale <119128221+Ranjitk0001@users.noreply.github.com>
2025-01-12 16:00:38 +0530

    Hospital Management System in services page

--- Projects/Hospital_Management_System/demo-hospital-managment-system/src/App.js
+++ Projects/Hospital_Management_System/demo-hospital-managment-system/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import HSpecialties from './Components/HSpecialties';
 import Services from './Components/Services';
 
 function App() {
   return (
    <>
-   <h1>Hospital Management System</h1>
+   {/* <h1>Hospital Management System</h1> */}
   <Services/>
+
+  <HSpecialties/>
    </>
   );
 }
