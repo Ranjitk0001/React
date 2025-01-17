4244e8f4a43c12dec77d4790a19f48c8f558202c
Ranjit Gajanan Kale <119128221+Ranjitk0001@users.noreply.github.com>
2025-01-17 18:28:46 +0530

    Backend to Connect Front End

--- Projects/LoginRegistration/loginregistration/src/main/java/com/login/loginregistration/loginregistration/controller/UsersController.java
+++ Projects/LoginRegistration/loginregistration/src/main/java/com/login/loginregistration/loginregistration/controller/UsersController.java
@@ -4,13 +4,12 @@ import com.login.loginregistration.loginregistration.entity.User;
 import com.login.loginregistration.loginregistration.request.LoginRequest;
 import com.login.loginregistration.loginregistration.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
+@CrossOrigin("*")
+//@CrossOrigin(origins = "http://localhost:3000")
 public class UsersController {
     @Autowired
     UserService userService;
