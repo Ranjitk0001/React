f98f9bef5f20ebea06943f3fe5ace4142745e7f0
Ranjit Gajanan Kale <119128221+Ranjitk0001@users.noreply.github.com>
2025-01-17 18:26:21 +0530

    Backend:- login
    post:- http://localhost:8084/api/v1/loginUser
    {
      "userId": "rk@example.com",
    
      "password": "Pass@123"
    }

--- Projects/LoginRegistration/loginregistration/src/main/java/com/login/loginregistration/loginregistration/controller/UsersController.java
+++ Projects/LoginRegistration/loginregistration/src/main/java/com/login/loginregistration/loginregistration/controller/UsersController.java
@@ -1,13 +1,16 @@
 package com.login.loginregistration.loginregistration.controller;
 
 import com.login.loginregistration.loginregistration.entity.User;
+import com.login.loginregistration.loginregistration.request.LoginRequest;
 import com.login.loginregistration.loginregistration.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1")
 public class UsersController {
     @Autowired
     UserService userService;
@@ -17,4 +20,12 @@ public class UsersController {
     public User addUser(@RequestBody User user) {
         return userService.addUser(user);
     }
+
+    @PostMapping("/loginUser")
+
+    public Boolean loginUser(@RequestBody LoginRequest loginRequest) {
+        return userService.loginUser(loginRequest);
+
+    }
+
 }
--- Projects/LoginRegistration/loginregistration/src/main/java/com/login/loginregistration/loginregistration/service/UserService.java
+++ Projects/LoginRegistration/loginregistration/src/main/java/com/login/loginregistration/loginregistration/service/UserService.java
@@ -2,8 +2,11 @@ package com.login.loginregistration.loginregistration.service;
 
 import com.login.loginregistration.loginregistration.entity.User;
 import com.login.loginregistration.loginregistration.repository.UsersRepo;
+import com.login.loginregistration.loginregistration.request.LoginRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -14,6 +17,23 @@ public class UserService {
     public User addUser(User user) {
 
         return usersRepo.save(user);
+
+    }
+
+
+    public Boolean loginUser(LoginRequest loginRequest) {
+
+        Optional<User> user = usersRepo.findById(loginRequest.getUserId());
+        User user1 = user.get();
+
+//        if(user1 == null) {
+//            return false;
+//        }
+        if(user1 == null || !user1.getPassword().equals(loginRequest.getPassword())) {
+            return false;
+        }
+
+        return true;
 
     }
 }
