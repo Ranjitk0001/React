f60cf7f0c94f2962e6f50b08899bb6d5516bb009
Ranjit Gajanan Kale <119128221+Ranjitk0001@users.noreply.github.com>
2025-01-17 18:48:48 +0530

    install bootstrap

--- Projects/LoginRegistration/loginregistration-frontend/src/App.js
+++ Projects/LoginRegistration/loginregistration-frontend/src/App.js
@@ -1,10 +1,14 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <>
-     <h1>  Login And Resitration </h1>
+      <h1> Login And Resitration </h1>
+      <div className="container">
+      
+        <h1 className="text-center">Hello, Bootstrap!</h1>
+        <button className="btn btn-primary">Click me</button>
+      </div>
     </>
   );
 }
