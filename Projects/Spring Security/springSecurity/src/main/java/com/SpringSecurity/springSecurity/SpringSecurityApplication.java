4d416d657589d613c3e0d18f2f82b8148e449b39
Ranjit Gajanan Kale <119128221+Ranjitk0001@users.noreply.github.com>
2025-01-18 16:18:01 +0530

    spring security basic run but Without security
    Web Spring ,Spring Boot DevTools

---
+++ Projects/Spring Security/springSecurity/src/main/java/com/SpringSecurity/springSecurity/SpringSecurityApplication.java
@@ -0,0 +1,15 @@
+package com.SpringSecurity.springSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityApplication.class, args);
+
+		System.out.println(" Welcome Spring Security");
+	}
+
+}
