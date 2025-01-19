21d96977ea2f65e4ccd91e5f196dd17e0c42ac07
Ranjit Gajanan Kale <119128221+Ranjitk0001@users.noreply.github.com>
2025-01-19 18:14:57 +0530

    jwt gernarate 5 RUN IT SUCCESSFULLY

---
+++ Projects/Spring Security/JWT Generated 5/userpassword4/src/main/java/com/springsecurity4/userpassword4/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.springsecurity4.userpassword4.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+    @GetMapping("/spring")
+    public String  SpringSecurity(HttpServletRequest request) {
+        return "csrf-springsecurity2 . Only web spring , Spring web tool" +
+                "json web token id is:-  "+request.getSession().getId();
+    }
+}
---
+++ Projects/Spring Security/JWT Generated 5/userpassword4/src/main/java/com/springsecurity4/userpassword4/service/UserService.java
@@ -0,0 +1,22 @@
+package com.springsecurity4.userpassword4.service;
+
+import com.springsecurity4.userpassword4.modelEntity.Users;
+import com.springsecurity4.userpassword4.repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepo repo;
+
+
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+    public Users register(Users user) {
+        user.setPassword(encoder.encode(user.getPassword()));
+        repo.save(user);
+        return user;
+    }
+}
