1cf4fc378ec00e234d363358abd771e6c7ab598b
Ranjit Gajanan Kale <119128221+Ranjitk0001@users.noreply.github.com>
2025-01-19 16:28:04 +0530

    you can gernerate password encoder means (Hash code)
    register user:-
    post :-http://localhost:8084/api/v1/register
    
    Insert data is :-
    {
            "id": 3,
            "username": "abhi",
            "password": "Pass@123"
        }
    
    show the data is:-
    {
        "id": 3,
        "username": "abhi",
        "password": "$2a$12$KiVfktrfYvZV90ZBqdimROEDNPa7f4m3UzRll3aiiltGdAu0yAIGG"
    }

--- Projects/Spring Security/userpassword4/src/main/java/com/springsecurity4/userpassword4/Userpassword4Application.java
+++ Projects/Spring Security/userpassword4/src/main/java/com/springsecurity4/userpassword4/Userpassword4Application.java
@@ -8,7 +8,7 @@ public class Userpassword4Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Userpassword4Application.class, args);
-		System.out.println("userpassword4");
+		System.out.println("userpassword4 + Encrypt and Decript password");
 	}
 
 }
---
+++ Projects/Spring Security/userpassword4/src/main/java/com/springsecurity4/userpassword4/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.springsecurity4.userpassword4.controller;
+
+import com.springsecurity4.userpassword4.modelEntity.Users;
+import com.springsecurity4.userpassword4.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+
+    @PostMapping("/register")
+    public Users register(@RequestBody Users user) {
+        return service.register(user);
+
+    }
+}
