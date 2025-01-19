be2a73c3d3fb0713444508ff7258235773d15a5e
Ranjit Gajanan Kale <119128221+Ranjitk0001@users.noreply.github.com>
2025-01-20 00:43:46 +0530

    jwt 7 :- Generate the Auto  Token

--- Projects/Spring Security/Generating JWT Token 6/userpassword4/src/main/java/com/springsecurity4/userpassword4/service/UserService.java
+++ Projects/Spring Security/Generating JWT Token 6/userpassword4/src/main/java/com/springsecurity4/userpassword4/service/UserService.java
@@ -34,7 +34,7 @@ public class UserService {
         Authentication authentication = authManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
         if (authentication.isAuthenticated()) {
 //            return "Success Authentication"  ;
-            return jwtService.generateToken();
+            return jwtService.generateToken(user.getUsername());
         }
         return "Fail";
     }
