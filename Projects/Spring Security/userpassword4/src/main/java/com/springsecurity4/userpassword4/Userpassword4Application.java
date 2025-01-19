0ab5867729d6111d987c757d5407ad96ffe91ea5
Ranjit Gajanan Kale <119128221+Ranjitk0001@users.noreply.github.com>
2025-01-19 13:41:21 +0530

    userpassword4  RUN IT

---
+++ Projects/Spring Security/userpassword4/src/main/java/com/springsecurity4/userpassword4/Userpassword4Application.java
@@ -0,0 +1,14 @@
+package com.springsecurity4.userpassword4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Userpassword4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Userpassword4Application.class, args);
+		System.out.println("userpassword4");
+	}
+
+}
